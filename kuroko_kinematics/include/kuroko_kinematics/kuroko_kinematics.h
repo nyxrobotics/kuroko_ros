bb9da09d4a6ea0a134216dd4acc2e13397d1cc12
Takaaki Numai <takaaki.numai01@g.softbank.co.jp>
2025-02-17 22:05:37 +0900

    Remove unused functions

--- kuroko_kinematics/include/kuroko_kinematics/kuroko_kinematics.h
+++ kuroko_kinematics/include/kuroko_kinematics/kuroko_kinematics.h
@@ -23,17 +23,6 @@ public:
   ~KurokoKinematics();
   KurokoKinematics(TreeSelect tree);
 
-  double getTotalMass(int joint_id);
-  Eigen::MatrixXd getMomentOfInertia(int joint_id);
-  Eigen::MatrixXd getCenterOfMass(const Eigen::MatrixXd& mc);
-
-  void solveForwardKinematics(int joint_ID);
-
-  Eigen::MatrixXd getJacobian(std::vector<int> idx);
-  Eigen::MatrixXd getJacobianCenterOfMass(std::vector<int> idx);
-  Eigen::MatrixXd computeStateError(const Eigen::MatrixXd& target_position, const Eigen::MatrixXd& current_position,
-                                    const Eigen::MatrixXd& target_orientation, Eigen::MatrixXd current_orientation);
-
   bool solveInverseKinematicsForRightLeg(std::vector<double>& joints_out, double x, double y, double z, double roll,
                                          double pitch, double yaw);
   bool solveInverseKinematicsForLeftLeg(std::vector<double>& joints_out, double x, double y, double z, double roll,
@@ -44,23 +33,18 @@ public:
   bool solveForwardKinematicsForLeftLeg(const std::vector<double>& joints_in, double& x, double& y, double& z,
                                         double& roll, double& pitch, double& yaw);
 
-  bool solveInverseKinematicsForRightArm(std::vector<double>& joints_out, double x, double y, double z,
-                                         double shoulder_pitch, double gripoper_open_angle);
-  bool solveInverseKinematicsForLeftArm(std::vector<double>& joints_out, double x, double y, double z,
-                                        double shoulder_pitch, double gripoper_open_angle);
-
-  LinkData* joint_link_tree_[ALL_JOINT_ID + 1];
-
-  LinkData* getLinkData(const std::string& link_name);
-  LinkData* getLinkData(const int link_id);
-  int getLinkIndex(const std::string& link_name);
   Eigen::MatrixXd getJointAxis(const std::string& link_name);
   double getJointDirection(const std::string& link_name);
   double getJointDirection(const int link_id);
-
   double leg_max_height_;
   double leg_side_offset_;
   double gripper_length_;
+
+private:
+  LinkData* joint_link_tree_[ALL_JOINT_ID + 1];
+  LinkData* getLinkData(const std::string& link_name);
+  LinkData* getLinkData(const int link_id);
+  int getLinkIndex(const std::string& link_name);
 };
 
 }  // namespace motion_control

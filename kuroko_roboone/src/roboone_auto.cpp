b490bc16970aa2d2496bbde968cb4bd1b9146c93
Takaaki Numai <takaaki.numai01@g.softbank.co.jp>
2025-09-01 20:26:41 +0900

    Refactoring

--- kuroko_roboone/src/roboone_auto.cpp
+++ kuroko_roboone/src/roboone_auto.cpp
@@ -663,7 +663,7 @@ void RobooneAuto::handleRun()
     ROS_WARN("Camera info is missing.");
     return;
   }
-  if (action_name_ != "")
+  if (!action_name_.empty())
   {
     return;
   }
@@ -671,29 +671,33 @@ void RobooneAuto::handleRun()
   bool roboone_found = false;
   int roboone_count = 0;
   jsk_recognition_msgs::Rect largest_rect;
+  jsk_recognition_msgs::Rect largest_conbined_rect;
   int largest_idx = -1;
 
   // robooneラベルを持つrectを探し、その中で一番大きいものを見つける
-  for (size_t i = 0; i < last_rects_.rects.size(); ++i)
+  if (ros::Time::now() - last_class_.header.stamp < ros::Duration(2.0) &&
+      ros::Time::now() - last_rects_.header.stamp < ros::Duration(2.0))
   {
-    if (last_class_.label_names[i] == "roboone")
+    for (size_t i = 0; i < last_rects_.rects.size(); ++i)
     {
-      if (last_rects_.rects[i].width > 10 && last_rects_.rects[i].height > 10)
+      if (last_class_.label_names[i] == "roboone")
       {
-        roboone_count++;
-        if (largest_rect.width * largest_rect.height < last_rects_.rects[i].width * last_rects_.rects[i].height)
+        if (last_rects_.rects[i].width > 10 && last_rects_.rects[i].height > 10)
         {
-          roboone_found = true;
-          robot_detected_time_ = ros::Time::now();  // robooneが見つかった時刻を記録
-          largest_rect = last_rects_.rects[i];
-          robot_detected_rect_ = largest_rect;
-          largest_idx = i;
+          roboone_count++;
+          if (largest_rect.width * largest_rect.height < last_rects_.rects[i].width * last_rects_.rects[i].height)
+          {
+            roboone_found = true;
+            robot_detected_time_ = ros::Time::now();
+            largest_rect = last_rects_.rects[i];
+            robot_detected_rect_ = largest_rect;
+            largest_idx = i;
+          }
         }
       }
     }
   }
 
-  jsk_recognition_msgs::Rect largest_conbined_rect;
   // 一番大きいrectに重なる部分があるrectを結合する
   if (roboone_found)
   {
@@ -726,11 +730,12 @@ void RobooneAuto::handleRun()
         }
       }
     }
+    robot_detected_rect_ = largest_rect;
   }
 
+  // 中心の上下左右10ピクセルがlargest_conbined_rectに内包されている場合は距離センサが有効
   bool range_available = false;
   int range_available_area_pixels = 10;
-  // 中心の上下左右10ピクセルがlargest_conbined_rectに内包されている場合は距離センサが有効
   if (roboone_found && ros::Time::now() - last_range_.header.stamp < ros::Duration(2.0) &&
       last_camera_info_.height / 2 - range_available_area_pixels > largest_conbined_rect.y &&
       last_camera_info_.height / 2 + range_available_area_pixels <
@@ -738,21 +743,18 @@ void RobooneAuto::handleRun()
       last_camera_info_.width / 2 - range_available_area_pixels > largest_conbined_rect.x &&
       last_camera_info_.width / 2 + range_available_area_pixels < largest_conbined_rect.x + largest_conbined_rect.width)
   {
-    range_available = true;
-    ROS_INFO("Range data is available: %f m", last_range_.range);
+    if (last_range_.range < 3.6 && last_range_.range > 0.001)
+    {
+      range_available = true;
+      ROS_INFO("Range data is available: %f m", last_range_.range);
+    }
   }
-
   last_rects_.rects.clear();
-
-  // アクション開始直後は処理をスキップ
-  if (!roboone_found && (ros::Time::now() - action_start_time_).toSec() < 0.3 && !action_name_.empty())
-    return;
 
   if (roboone_found || (ros::Time::now() - robot_detected_time_).toSec() < 2.0)
   {
     double rect_area = (robot_detected_rect_.width * robot_detected_rect_.height) /
                        double(last_camera_info_.width * last_camera_info_.height);
-    // ROS_INFO("Largest roboone rect found with area: %f", rect_area);
     double rect_center_x = robot_detected_rect_.x + robot_detected_rect_.width / 2.0;
     double rect_center_y = robot_detected_rect_.y + robot_detected_rect_.height / 2.0;
     double image_center_x = last_camera_info_.width / 2.0;
@@ -766,17 +768,16 @@ void RobooneAuto::handleRun()
       force_walk_ = false;
     }
 
-    if ((robot_detected_rect_.y > last_camera_info_.height * 0.6 && rect_area > atk_min_rect_size_ * 0.3 &&
-         rect_area < atk_min_rect_size_) ||
-        roboone_count > 2)
+    // 相手ロボットが画面の一定以下のときは後退（相手が転倒していて至近距離の場合）
+    if (robot_detected_rect_.y > last_camera_info_.height * 0.6 &&
+        (rect_area > atk_min_rect_size_ * 0.66 || (range_available && last_range_.range > attack_distance_ * 1.5)))
     {
-      // 相手ロボットが画面のした半分にしか入っていたいときはなにかおかしいので後退
-      ROS_INFO_THROTTLE(1.0, "Target is too low, retreating.");
+      ROS_INFO_THROTTLE(3.0, "The target is down and close. Move back.");
       double target_direction = (x_offset > 0) ? -1.0 : 1.0;  // 右なら-1、左なら1
-      double target_angle_factor = -x_offset;
-      if (target_angle_factor > 0.5)
+      double target_angle_factor = -x_offset * 2.0;
+      if (target_angle_factor > 1.0)
         target_angle_factor = 1.0;
-      else if (target_angle_factor < -0.5)
+      else if (target_angle_factor < -1.0)
         target_angle_factor = -1.0;
       double yaw_step = target_angle_factor * fabs(yaw_step_max_);
       double x_step = -fabs(x_backward_step_max_) * (1.0 - fabs(target_angle_factor));
